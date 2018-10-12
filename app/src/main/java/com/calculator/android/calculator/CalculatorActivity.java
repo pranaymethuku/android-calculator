fdadbb4b9039ff7522ef984543696ade7f3a4dac
Pranay Methuku <pranaymethuku@gmail.com>
2018-10-12 15:32:50 -0400

    Rename MainActivity to CalculatorActivity, activity_main to activity_calculator

--- app/src/main/java/com/calculator/android/calculator/CalculatorActivity.java
+++ app/src/main/java/com/calculator/android/calculator/CalculatorActivity.java
@@ -3,11 +3,11 @@ package com.calculator.android.calculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CalculatorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_calculator);
     }
 }
